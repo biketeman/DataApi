f7b4ac020dcdd3fe5d4439ace4a45b6bd63ab19c
Alexis Lagniel <a_lagniel@etu-webschoolfactory.fr>
2019-03-20 16:06:01 +0100

    ending schemas completion

--- backofficeData/index.js
+++ backofficeData/index.js
@@ -1,5 +1,5 @@
 const {GraphQLServer} = require('graphql-yoga');
-const { queryType, stringArg, makeSchema,	objectType} = require('nexus');
+const { queryType, stringArg, makeSchema,objectType} = require('nexus');
 const { pg } = require('pg');
 const knex = require('knex');
 
@@ -7,10 +7,12 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(`./config/${env}`)
 const db = config.DB;
 const Segment = require(`./types/segments.js`);
-const Client = require(`./types/clients.js`)
-const CarteComm = require(`./types/cartecomm.js`)
+const Client = require(`./types/clients.js`);
+const CarteComm = require(`./types/cartecomm.js`);
+const Abofrequences = require(`./types/abofrequences.js`);
+const Abotgvmax = require(`./types/abotgvmax.js`);
 
-
+Abotgvmax
 
 
 const Query = queryType({
@@ -27,7 +29,7 @@ const Query = queryType({
 
 
 const schema = makeSchema({
-	types: [Query, Segment, Client, CarteComm],
+	types: [Query, Segment, Client, CarteComm, Abofrequences, Abotgvmax],
 	outputs: {
 		schema: __dirname + "/generated/schema.graphql",
 		typegen: __dirname + "/generated/typings.ts",
